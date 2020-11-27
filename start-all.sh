2fc32603bb9541f1946f746366473798190e1351
Jan Gabriel <spanwair@gmail.com>
2020-11-27 23:13:36 +0100

    date-2020-11-27_23:13:36

--- start-all.sh
+++ start-all.sh
@@ -6,6 +6,7 @@ do
   sh data.sh 00012866 csvs-jindra-home csvs-parts-jindra-home;
   sh data.sh 00012908 csvs-zs-17-listopadu csvs-parts-zs-17-listopadu;
   sh data.sh 00012909 csvs-zs-hornicka csvs-parts-zs-hornicka;
+  sh gitdeleteremote.sh
   date;
   sleep $timeout;
 done;
