8be8d9810748bc39f56ed48b4e86871068ea97a2
Jan Gabriel <spanwair@gmail.com>
2020-11-27 21:44:52 +0100

    date-2020-11-27_21:44:52

--- start-jindra-home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-id=00012866; # device id of school = 00012908
-mainfolder="csvs-jindra-home"; # main folder
-partsfolder="csvs-parts-jindra-home"; # main folder parts
-sh start.sh $id $mainfolder $partsfolder yes;
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# timeout=${1:-86400};
-timeout=120;
-while true; do sh data.sh $1 $2 $3 $4; date; sleep $timeout; done;
