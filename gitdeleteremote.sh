6a30bc1d664bbb2b78de5435f9821b7db6ced35c
VaQhI6ZJ589hzJ7szaUiSpWXXANbcvmLhEtHlW2lH3s <VaQhI6ZJ589hzJ7szaUiSpWXXANbcvmLhEtHlW2lH3s@mailpoof.com>
2020-11-27 23:53:23 +0100

    date-2020-11-27_23:53:23

--- gitdeleteremote.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-GIT='git'
-# $GIT update-ref -d HEAD
-$GIT add .
-$GIT rm --cached -f .gitignore cookies credentials data.sh git.sh start-jindra-home.sh start-zs-17-listopadu.sh start-zs-hornicka.sh start.sh test.sh gitdeleteremote.sh gitNotUsed.sh start-all.sh
-$GIT rm --cached -f -r files/csvs
-$GIT commit -m date-`date +%Y-%m-%d_%H:%M:%S`
-$GIT push -u remote master -f
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# awk -F, 'FNR==NR{a[$1]=$2;next} $1 in a{$3=a[$1]} 1' OFS=, csvs/*.csv > new.csv
-awk '!a[$0]++' csvs/*.csv > csvs/0-main.csv
