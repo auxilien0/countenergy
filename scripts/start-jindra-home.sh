da46bb7ae02fda4e2d79fc81cd6ef7dd793d3fcb
Jan Gabriel <spanwair@gmail.com>
2020-12-01 00:08:04 +0100

    date-2020-12-01_00:08:04

---
+++ scripts/start-jindra-home.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+id=00012866; # device id of school
+mainfolder="csvs-jindra-home"; # main folder
+partsfolder="csvs-parts-jindra-home"; # main folder parts
+sh ../start.sh $id $mainfolder $partsfolder;
