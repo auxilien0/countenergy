4e3814771d42affbc0d5bf143556c17a41fe535f
Jan Gabriel <spanwair@gmail.com>
2020-12-01 00:07:33 +0100

    date-2020-12-01_00:07:33

--- scripts/start-zs-17-listopadu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-id=00012908; # device id of school
-mainfolder="csvs-zs-17-listopadu"; # main folder
-partsfolder="csvs-parts-zs-17-listopadu"; # main folder parts
-sh ../start.sh $id $mainfolder $partsfolder;
--- scripts/start-zs-hornicka.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-id=00012909; # device id of school
-mainfolder="csvs-zs-hornicka"; # main folder
-partsfolder="csvs-parts-zs-hornicka"; # main folder parts - OPTIONAL
-sh ../start.sh $id $mainfolder $partsfolder;
