f30981e4b55dbaa0a8d5249547bb8c031ac8b7eb
Jan Gabriel <spanwair@gmail.com>
2020-11-30 23:15:32 +0100

    date-2020-11-30_23:15:32

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+exec ./start-all.sh
 # awk -F, 'FNR==NR{a[$1]=$2;next} $1 in a{$3=a[$1]} 1' OFS=, csvs/*.csv > new.csv
 # awk '!a[$0]++' csvs/*.csv > csvs/0-main.csv
-if [ ! -n "$(git status --porcelain files/csvs-parts)" ]; then
-  echo "changed";
-else
-  echo "no";
-fi
+# if [ ! -n "$(git status --porcelain files/csvs-parts)" ]; then
+#   echo "changed";
+# else
+#   echo "no";
+# fi
