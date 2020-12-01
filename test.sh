40cbfc23dc0f5844af823bd023d23fc310dbbf7d
Jan Gabriel <spanwair@gmail.com>
2020-12-01 21:33:46 +0100

    date-2020-12-01_21:33:46

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exec ./start-all.sh;
+# exec ./start-all.sh;
 # awk -F, 'FNR==NR{a[$1]=$2;next} $1 in a{$3=a[$1]} 1' OFS=, csvs/*.csv > new.csv
 # awk '!a[$0]++' csvs/*.csv > csvs/0-main.csv
 # if [ ! -n "$(git status --porcelain files/csvs-parts)" ]; then
@@ -7,3 +7,8 @@ exec ./start-all.sh;
 # else
 #   echo "no";
 # fi
+
+while IFS=\; read -r col1
+do
+  echo "$col1"
+done < 00012866.csv
