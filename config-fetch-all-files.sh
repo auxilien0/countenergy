af0eacfdc3e762611b06b942b269a9e2584ee49d
Jan Gabriel <spanwair@gmail.com>
2020-12-01 21:57:48 +0100

    date-2020-12-01_21:57:48

--- config-fetch-all-files.sh
+++ config-fetch-all-files.sh
@@ -3,9 +3,9 @@
 GIT='git';
 $GIT fetch ${1:-remote} master;
 $GIT reset --hard FETCH_HEAD;
-for f in scripts/start-*.sh; do
-  name=$(echo $f | grep -P -o '(?<=start-).*(?=\.sh)');
-  id=$(grep -E -o "[0-9]+" scripts/start-$name.sh);
-  cat files/csvs-parts/csvs-parts-$name/part_*.csv > "files/csvs/csvs-$name/$id.csv";
-done
+# for f in scripts/start-*.sh; do
+#   name=$(echo $f | grep -P -o '(?<=start-).*(?=\.sh)');
+#   id=$(grep -E -o "[0-9]+" scripts/start-$name.sh);
+#   cat files/csvs-parts/csvs-parts-$name/part_*.csv > "files/csvs/csvs-$name/$id.csv";
+# done
 
