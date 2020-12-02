cbb9d5fe9a95b4566d4f3445d0cbcdc9db1eddad
VaQhI6ZJ589hzJ7szaUiSpWXXANbcvmLhEtHlW2lH3s <VaQhI6ZJ589hzJ7szaUiSpWXXANbcvmLhEtHlW2lH3s@mailpoof.com>
2020-12-02 12:29:48 +0100

    date-2020-12-02_12:29:48

--- login-fetch-cookies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# login and fetch cookies
-wget --save-cookies cookies --keep-session-cookies --post-data 'username=Gabriel&password=Zoubek2020&submit=+Enter+%C2%BB+' --delete-after http://sensorfor.com/cloud/login.php
